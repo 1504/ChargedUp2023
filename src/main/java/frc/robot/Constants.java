370700309d3324e7cd8f0824e6e08330189ad558
xuz24 <zijie.xu06@gmail.com>
2023-03-15 19:13:39 -0400

    Add shuffleboard manager

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -131,7 +131,7 @@ public final class Constants {
 
   public static class ArmConstants {
     // TODO: idk any of these numbers
-    public static final int kMotorPort = 20;
+    public static final int kMotorPort = 21;
 
     public static final double kP = 0;
 
